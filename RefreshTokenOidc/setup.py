5cc71cbaa04318611f154963e21bec336e7628f6
quinoescobar <quinoescobar@gmail.com>
2017-06-17 18:26:07 +0100

    añadidos otros dirs, licencia y version

---
+++ RefreshTokenOidc/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='',
+    version='',
+    author='',
+    author_email='',
+    packages=['',''],
+    scripst=['',''],
+    url='',
+    license='',
+    description='',
+    long_description=,
+    install_require=['',''],
+)
