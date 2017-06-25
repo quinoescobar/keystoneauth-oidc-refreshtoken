87d9dae36205e6923254764aceb27afe562ec549
quinoescobar <quinoescobar@gmail.com>
2017-06-25 21:17:53 +0100

    just trying 2

--- keystoneauth_oidc_refreshtoken/tests/unit/test_oidc.py
+++ keystoneauth_oidc_refreshtoken/tests/unit/test_oidc.py
@@ -19,7 +19,7 @@
 # import uuid
 from keystoneauth1.tests.unit.identity import test_identity_v3_oidc
 from keystoneauth1.tests.unit import oidc_fixtures
-# from keystoneauth1.tests.unit import utils
+from keystoneauth1.tests.unit import utils
 
 # from keystoneauth_oidc_refreshtoken.tests.unit import oidc_fixtures
 from keystoneauth_oidc_refreshtoken import plugin as oidc
